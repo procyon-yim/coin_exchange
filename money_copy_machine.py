ee49ad93c5236ce7cd493e509eba14b8b9e4bd6b
procyon-yim <69961142+procyon-yim@users.noreply.github.com>
2021-10-17 23:03:59 +0100

    Translated Korean to English

--- money_copy_machine.py
+++ money_copy_machine.py
@@ -1,18 +1,16 @@
 from coin_exchanger import *
 
 username, password = login('upbit.txt')
-jaebeom = pyupbit.Upbit(username, password)
+user = pyupbit.Upbit(username, password)
 
-# 주문 외 Exchange API는 초당 30회 호출 가능. (https://github.com/sharebook-kr/pyupbit 참고)
-
-coins = ['KRW-BTC', 'KRW-ETH', 'KRW-XRP', 'KRW-ADA', 'KRW-DOGE']
-k = 0.5
+coins = ['KRW-BTC', 'KRW-ETH', 'KRW-XRP', 'KRW-ADA', 'KRW-DOGE']  # list of coins to exchange
+k = 0.5  # breakout coefficient (see https://www.whselfinvest.com/en-lu/trading-platform/free-trading-strategies/tradingsystem/56-volatility-break-out-larry-williams-free)
 amount = get_amount(coins)
-target_price = get_target_price(coins, k)  # 목표가 계산하는데 1초 소요
-start_balance  = jaebeom.get_balance()  # 이 돈을 가지고 시작하는거다.
+target_price = get_target_price(coins, k)
+start_balance  = user.get_balance()
 now = datetime.datetime.now()
 mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(days=1)
-send_alarm('mail.txt', '현재 시각 {2}, 현재 잔고 {0}KRW. 매수 비중은 {1}.'.format(int(start_balance), amount, now))
+send_alarm('mail.txt', 'Current time is {2}, Current Balance is {0}KRW. Today\'s targets are {1}.'.format(int(start_balance), amount, now))
 
 try:
     while True:
@@ -20,60 +18,59 @@ try:
 
         if mid < now < mid + datetime.timedelta(seconds=10):
             try:
-                renew(jaebeom, jaebeom.get_balances())
-
+                renew(user, user.get_balances())
                 coins = ['KRW-BTC', 'KRW-ETH', 'KRW-XRP', 'KRW-ADA', 'KRW-DOGE']
                 k = 0.5
                 amount = get_amount(coins)
                 target_price = get_target_price(coins, k)
-                start_balance = jaebeom.get_balance()
+                start_balance = user.get_balance()
                 now = datetime.datetime.now()
                 mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(days=1)
-                send_alarm('mail.txt', '현재 시각 {2}, 현재 잔고 {0}KRW. 매수 비중은 {1}.'.format(int(start_balance), amount, now))
+                send_alarm('mail.txt', 'Current time is {2}, Current Balance is {0}KRW. Today\'s targets are {1}.'.format(int(start_balance), amount, now))
 
             except TypeError:
-                send_alarm('mail.txt', '자정 프로세스 중 API를 너무 많이 호출했습니다 (JSONDecodeError). MCM을 종료합니다.')
+                send_alarm('mail.txt', 'JSONDecodeError. Terminating MCM.')
                 break
 
             except Exception as e:
-                send_alarm('mail.txt', '자정 프로세스 중 에러 발생. {} MCM을 종료합니다.'.format(e))
+                send_alarm('mail.txt', 'An error occured during 00:00 process. {} Terminating MCM.'.format(e))
                 break
 
         try:
             for coin in coins:
                 current_price = pyupbit.get_current_price(coin)
-                time.sleep(0.1)  # (quotation api는 초당 10회 가능)
+                time.sleep(0.1)  # This is because you can call an Upbit API only 10 times a second
 
                 if current_price >= target_price[coin]:
                     qtty = amount[coin]
-                    time.sleep(0.1)  # json error 방지
+                    time.sleep(0.1)
 
                     if qtty == 0:
                         coins.remove(coin)
 
                     else:
-                        jaebeom.buy_market_order(coin, start_balance * qtty)
-                        time.sleep(0.1)  # json error 방지
+                        user.buy_market_order(coin, start_balance * qtty)
+                        time.sleep(0.1)
                         coins.remove(coin)
 
         except:
             try:
                 for coin in coins:
                     current_price = pyupbit.get_current_price(coin)
-                    time.sleep(0.5)  # (quotation api는 초당 10회 가능)
+                    time.sleep(0.5)
 
                     if current_price >= target_price[coin]:
                         qtty = amount[coin]
-                        time.sleep(0.5)  # json error 방지
+                        time.sleep(0.5)
                         if qtty == 0:
                             coins.remove(coin)
                         else:
-                            jaebeom.buy_market_order(coin, start_balance * qtty)
-                            time.sleep(0.5)  # json error 방지
+                            user.buy_market_order(coin, start_balance * qtty)
+                            time.sleep(0.5)
                             coins.remove(coin)
 
             except Exception as e:
-                send_alarm('mail.txt', "매매 프로세스 중 에러 발생. {}. MCM을 종료합니다.".format(e))
+                send_alarm('mail.txt', "An error occured while exchanging. {}. Terminating MCM.".format(e))
                 break
 
         time.sleep(1)
--- reset.py
+++ reset.py
@@ -1,6 +1,6 @@
 from coin_exchanger import *
 
 username, password = login('upbit.txt')
-jaebeom = pyupbit.Upbit(username, password)
+user = pyupbit.Upbit(username, password)
 
-renew(jaebeom, jaebeom.get_balances())
+renew(user, user.get_balances())
